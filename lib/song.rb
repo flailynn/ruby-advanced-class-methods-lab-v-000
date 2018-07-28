e4b827e784f67ff990ed2bff01436efeeadc680a
Anita Beauchamp <amadamspr@gmail.com>
2018-07-28 15:27:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,11 @@ class Song
   end
 
   def self.create
+    #create a new song instance
     song_instance = self.new
+    #save song
     song_instance.save
+    #return new song that was created
     song_instance
   end
 
