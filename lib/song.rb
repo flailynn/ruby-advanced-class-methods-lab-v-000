82676605926e8937988874938bb9b458fb60b72d
Anita Beauchamp <amadamspr@gmail.com>
2018-07-28 15:50:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,17 +39,20 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    song ||= self.create_by_name(name)
+=begin
     if self.find_by_name(name)
       song = self.find_by_name(name)
     else
       song = self.create_by_name(name)
     end
     song
-
+=end
   end
 
   def self.alphabetical
-    
+
   end
 
 end
